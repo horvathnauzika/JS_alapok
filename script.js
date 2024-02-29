6305ea268b31c0bd7061f34d225e589629c6684d
horvathnauzika <naihorvath@gmail.com>
2024-02-29 12:16:22 +0100

    folytatas

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
+    elemekElerese4();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+    const ELEM=document.querySelectorAll("section h2")[0]
+    console.log(ELEM.innerHTML)
 }
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
+function elemekElerese2() {
+    const ELEM=document.getElementById("ide")
+    ELEM.innerHTML += "<P>Jó reggelt!</p>"
 }
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
+function elemekElerese3() {
+    const ELEM=document.querySelectorAll("#ide")[0]
+    ELEM.innerHTML += "<P>Jó reggelt!</p>"
+}
+
+function elemekElerese4(){
+    const ELEM=document.getElementsByClassName("lista")
+    let txt = "<ul>"
+    for (let index = 0; index < 5; index++) {
+        txt += "<li>" + Math.random(10-30)+10 + "</li>"
+    }
+    txt = "</ul>"
+    ELEM.innerHTML += txt
 }
